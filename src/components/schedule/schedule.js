6acad3490903a6bcf1aedc22a027fd11b0a8662e
Siddhesh Dhage <82159089+sidDGP@users.noreply.github.com>
2021-09-23 21:42:34 +0100

    Adding the scheduler

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -1,7 +1,47 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './schedule.css';
+
 function Schedule() {
   return (
-    <div className="Schedule">
-    </div>
+    <Container fluid>
+      <Row className="Monday Weekday">
+        <Col>1 of 3</Col>
+        <Col>2 of 3 (wider)</Col>
+        <Col>3 of 3</Col>
+      </Row>
+      <Row className="Tuesday Weekday">
+        <Col>1 of 3</Col>
+        <Col>2 of 3 (wider)</Col>
+        <Col>3 of 3</Col>
+      </Row>
+      <Row className="Wednesday Weekday">
+        <Col>1 of 3</Col>
+        <Col>2 of 3 (wider)</Col>
+        <Col>3 of 3</Col>
+      </Row>
+      <Row className="Thursday Weekday">
+        <Col>1 of 3</Col>
+        <Col>2 of 3 (wider)</Col>
+        <Col>3 of 3</Col>
+      </Row>
+      <Row className="Friday Weekday">
+        <Col>1 of 3</Col>
+        <Col>2 of 3 (wider)</Col>
+        <Col>3 of 3</Col>
+      </Row>
+      <Row className="Saturday Weekday">
+        <Col>1 of 3</Col>
+        <Col>2 of 3 (wider)</Col>
+        <Col>3 of 3</Col>
+      </Row>
+      <Row className="Sunday Weekday">
+        <Col>1 of 3</Col>
+        <Col>2 of 3 (wider)</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
   );
 }
 

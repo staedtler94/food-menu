8284e8503690ce87a74af83981ad9268b328694f
Siddhesh Dhage <siddheshdhage@yahoo.com>
2021-10-10 19:36:09 +0100

    Improving the scheduling component

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -1,46 +1,53 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import './schedule.css';
 
 function Schedule() {
+
+  const daysOfWeek = [{
+    day: 'Monday',
+  }, {
+    day: 'Tuesday',
+  }, {
+    day: 'Wednesday',
+  }, {
+    day: 'Thursday',
+  }, {
+    day: 'Friday',
+  }, {
+    day: 'Saturday',
+  }, {
+    day: 'Sunday',
+  }];
+
+  const tabuledWeek = daysOfWeek.map((dayEl) => {
+    const classNameDay = dayEl.day + " day";
+     return (<Row className={classNameDay}>
+          <Col md={3} lg={3}>{dayEl.day}</Col>
+          <Col md={3} lg={3}>
+            <Button className="pull-right" variant="primary" size="sm">
+              Add
+            </Button>
+          </Col>
+          <Col md={3} lg={3}>3 of 3</Col>
+          <Col md={3} lg={3}>3 of 3</Col>
+        </Row>)
+  });
+
+  const headers = (()=>{return (<Row>
+        <Col md={3} lg={3}>Days</Col>
+        <Col md={3} lg={3}>Breakfast</Col>
+        <Col md={3} lg={3}>Lunch</Col>
+        <Col md={3} lg={3}>Dinner</Col>
+     </Row>)
+   })()
+
   return (
     <Container fluid>
-      <Row className="Monday Weekday">
-        <Col>1 of 3</Col>
-        <Col>2 of 3 (wider)</Col>
-        <Col>3 of 3</Col>
-      </Row>
-      <Row className="Tuesday Weekday">
-        <Col>1 of 3</Col>
-        <Col>2 of 3 (wider)</Col>
-        <Col>3 of 3</Col>
-      </Row>
-      <Row className="Wednesday Weekday">
-        <Col>1 of 3</Col>
-        <Col>2 of 3 (wider)</Col>
-        <Col>3 of 3</Col>
-      </Row>
-      <Row className="Thursday Weekday">
-        <Col>1 of 3</Col>
-        <Col>2 of 3 (wider)</Col>
-        <Col>3 of 3</Col>
-      </Row>
-      <Row className="Friday Weekday">
-        <Col>1 of 3</Col>
-        <Col>2 of 3 (wider)</Col>
-        <Col>3 of 3</Col>
-      </Row>
-      <Row className="Saturday Weekday">
-        <Col>1 of 3</Col>
-        <Col>2 of 3 (wider)</Col>
-        <Col>3 of 3</Col>
-      </Row>
-      <Row className="Sunday Weekday">
-        <Col>1 of 3</Col>
-        <Col>2 of 3 (wider)</Col>
-        <Col>3 of 3</Col>
-      </Row>
+      {headers}
+      {tabuledWeek}
     </Container>
   );
 }

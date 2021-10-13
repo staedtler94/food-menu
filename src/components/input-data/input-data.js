5c77206242d134bfb3bbf2eb73b4aa29f86eacd5
Siddhesh Dhage <siddheshdhage@yahoo.com>
2021-10-13 19:08:25 +0100

    updating the menu input page

--- src/components/input-data/input-data.js
+++ src/components/input-data/input-data.js
@@ -1,17 +1,22 @@
-import InputGroup from 'react-bootstrap/InputGroup'
-import FormControl from 'react-bootstrap/FormControl'
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import './input-data.css';
 // this is the input for the food in scheduler
 function InputData(props){
 
   return (
+    <div>
     <InputGroup className="mb-3">
       <FormControl
         placeholder="Enter Food Name"
         aria-label="FoodName"
         aria-describedby="basic-addon1"
         />
-        <InputGroup.Text id="basic-addon1">Pick</InputGroup.Text>
+        <Button variant="outline-secondary" id="button-addon2"> Pick </Button>
     </InputGroup>
+
+    </div>
   )
 }
 
--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -2,48 +2,70 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import dayjs from "dayjs";
+import weekOfYear from 'dayjs/plugin/weekOfYear';
 import './schedule.css';
 import InputData from './../input-data/input-data';
 
 function Schedule() {
 
+  dayjs.extend(weekOfYear);
+  const weekDayNumber = dayjs().day();
+
+  let sunday;
+  if(weekDayNumber === 0)
+    sunday = dayjs();
+  else {
+      sunday = dayjs().subtract(weekDayNumber, 'day');
+  }
+
+  console.log(sunday);
+
   const daysOfWeek = [{
     day: 'Monday',
+    date: sunday.add(1, 'day')
   }, {
     day: 'Tuesday',
+    date: sunday.add(2, 'day')
   }, {
     day: 'Wednesday',
+    date: sunday.add(3, 'day')
   }, {
     day: 'Thursday',
+    date: sunday.add(4, 'day')
   }, {
     day: 'Friday',
+    date: sunday.add(5, 'day')
   }, {
     day: 'Saturday',
+    date: sunday.add(6, 'day')
   }, {
     day: 'Sunday',
+    date: sunday.add(7, 'day')
   }];
+
 
   const tabuledWeek = daysOfWeek.map((dayEl) => {
     const classNameDay = dayEl.day + " day";
      return (<Row className={classNameDay}>
-          <Col md={1} lg={1}>{dayEl.day}</Col>
+          <Col md={2} lg={2}>{dayEl.day} ({dayjs(dayEl.date).format('DD, MMM YY')})</Col>
           <Col md={3} lg={3}>
             <InputData className="input-class"/>
           </Col>
-          <Col md={{ span:3, offset:1}} lg={{ span:3, offset:1}}>
+          <Col md={3} lg={3}>
             <InputData className="input-class"/>
           </Col>
-          <Col md={{ span:3, offset:1}} lg={{ span:3, offset:1}}>
+          <Col md={3} lg={3}>
             <InputData />
           </Col>
         </Row>)
   });
 
   const headers = (()=>{return (<Row>
-        <Col md={1} lg={1}>Days</Col>
+        <Col md={2} lg={2}>Days</Col>
         <Col md={3} lg={3}>Breakfast</Col>
-        <Col md={{ span:3, offset:1}} lg={{ span:3, offset:1}}>Lunch</Col>
-        <Col md={{ span:3, offset:1}} lg={{ span:3, offset:1}}>Dinner</Col>
+        <Col md={3} lg={3}>Lunch</Col>
+        <Col md={3} lg={3}>Dinner</Col>
      </Row>)
    })()
 
@@ -51,6 +73,7 @@ function Schedule() {
     <Container fluid>
       {headers}
       {tabuledWeek}
+
     </Container>
   );
 }
